84697b57d3c96a3dc8267bda7fb9e6da334b50b7
Gabriel Suassuna Almeida <gabrielsuassuna.05@gmail.com>
2021-04-03 05:21:52 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+
+  const dataset = d3.json("").then((data) => data);
+
+  console.log(dataset);
+</script>
+
+<main>
+  {console.log(dataset)}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
