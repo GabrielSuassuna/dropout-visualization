d3adff6623bcfbe9895e42d082b6134104304b5c
Gabriel Suassuna Almeida <gabrielsuassuna.05@gmail.com>
2021-04-03 05:33:46 -0300

    Change data

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,14 @@
   import { onMount } from "svelte";
   import * as d3 from "d3";
 
-  const dataset = d3.json("").then((data) => data);
-
-  console.log(dataset);
+  const dataset = d3
+    .json(
+      "https://raw.githubusercontent.com/GabrielSuassuna/dropout-visualization/master/data/data.json?token=AIYBXOUMBRZ6DJ6PZMAAGC3AOFQCY#"
+    )
+    .then((data) => console.log(data));
 </script>
 
-<main>
-  {console.log(dataset)}
-</main>
+<main />
 
 <style>
   main {
